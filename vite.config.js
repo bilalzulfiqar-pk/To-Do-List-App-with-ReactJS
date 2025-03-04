696f4c30e1bac5a14854e4add0166937b9a0f62d
Muhammad Bilal Zulfiqar <bilalzulfiqar34@gmail.com>
2025-03-04 16:09:51 +0500

    Added PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate", // Auto updates the service worker
+      devOptions: {
+        enabled: true, // Enables PWA in development
+      },
+      manifest: {
+        name: "To-Do list App",
+        short_name: "App",
+        description: "A simple and efficient to-do list app to organize your tasks and boost productivity",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
